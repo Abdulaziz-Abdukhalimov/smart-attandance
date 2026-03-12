25df9a34cd806402c38d43ad52d0c1d2fbd845a2
Abdulaziz Abdukhalimov <abdukhalimov_front_end@Abdulazizs-MacBook-Pro.local>
2026-03-13 00:02:48 +0900

    feat: Basic project structures

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { TeacherModule } from './modules/teacher/teacher.module';
+import { StudentModule } from './modules/student/student.module';
+import { ScheduleModule } from './modules/schedule/schedule.module';
+import { AttendanceModule } from './modules/attendance/attendance.module';
+import { SchoolModule } from './modules/school/school.module';
+import { ClassModule } from './modules/class/class.module';
+import { SubjectModule } from './modules/subject/subject.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TeacherModule, StudentModule, ScheduleModule, AttendanceModule, SchoolModule, ClassModule, SubjectModule],
   controllers: [AppController],
   providers: [AppService],
 })
